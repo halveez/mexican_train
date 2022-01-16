d71ff0398bd2793fad185f7fbd33dd9993fb6bc3
Zachary Halvorson <zthalv@bu.edu>
2022-01-15 16:57:43 -0800

    Minor changes

--- solver.py
+++ solver.py
@@ -4,9 +4,18 @@ import random
 domino_bank = [[0,0],[0,1],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[0,8],[0,9],[0,10],[0,11],[0,12],[1,1],[1,2],[1,3],[1,4],[1,5],[1,6],[1,7],[1,8],[1,9],[1,10],[1,11],[1,12],[2,2],[2,3],[2,4],[2,5],[2,6],[2,7],[2,8],[2,9],[2,10],[2,11],[2,12],[3,3],[3,4],[3,5],[3,6],[3,7],[3,8],[3,9],[3,10],[3,11],[3,12],[4,4],[4,5],[4,6],[4,7],[4,8],[4,9],[4,10],[4,11],[4,12],[5,5],[5,6],[5,7],[5,8],[5,9],[5,10],[5,11],[5,12],[6,6],[6,7],[6,8],[6,9],[6,10],[6,11],[6,12],[7,7],[7,8],[7,9],[7,10],[7,11],[7,12],[8,8],[8,9],[8,10],[8,11],[8,12],[9,9],[9,10],[9,11],[9,12],[10,10],[10,11],[10,12],[11,11],[11,12],[12,12]]
 
 class DominoTree:
-	def __init__(self, value):
+	def __init__(self, value, children = []):
 		self.value = value
-		self.children = []
+		self.children = children
+
+	def __str__(self, level=0):
+		ret = "\t"*level+repr(self.value)+"\n"
+		for child in self.children:
+			ret += child.__str__(level+1)
+		return ret
+
+	def __repr__(self):
+		return '<tree node representation>'
 
 	def add_child(self, insert):
 		self.children.append(insert)
@@ -68,6 +77,16 @@ def hand_arranger(hand_list, starting_domino):
 
 	#return arranged_hand
 
+def match_bool(current_domino, hand_list):
+	# Label the current hand whether it can be played or not
+	# 1 for can play domino as is
+	# 0 for cannot
+	# -1 for can play domino in reverse position
+	# Example:
+	# Current domino ends in a [4]
+	# Hand List = [[12|4], [1|5], [0,3], [4,4], [4,6]]
+	# Returned List = [-1, 0, 0, ?, 1]
+
 
 if __name__ == "__main__":
 
@@ -82,68 +101,17 @@ if __name__ == "__main__":
 	test_hand = [[1, 2], [1, 12], [3, 4], [3, 9], [3, 10], [4, 7], [5, 7], [5, 11], [6, 10], [7, 12], [8, 9], [8, 12]]
 	start = DominoTree(12)
 
-	a = DominoTree(1)
-	b = DominoTree(7)
-	c = DominoTree(8)
+	start.children = [DominoTree(1), DominoTree(7), DominoTree(8)]
 
-	start.add_child(a)
-	start.add_child(b)
-	start.add_child(c)
+	start.children[0].children = [DominoTree(2)]
+	start.children[1].children = [DominoTree(4), DominoTree(5)]
+	start.children[2].children = [DominoTree(9)]
 
-	d = DominoTree(2)
-	e = DominoTree(4)
-	f = DominoTree(5)
-	g = DominoTree(9)
+	start.children[1].children[0].children = [DominoTree(3)]
+	start.children[1].children[1].children = [DominoTree(11)]
+	start.children[2].children[0].children = [DominoTree(3)]
 
-	a.add_child(d)
-	b.add_child(e)
-	b.add_child(f)
-	c.add_child(g)
+	start.children[1].children[0].children[0].children = [DominoTree(9), DominoTree(10)]
+	start.children[2].children[0].children[0].children = [DominoTree(10), DominoTree(4)]
 
-	h = DominoTree(3)
-	i = DominoTree(11)
-	j = DominoTree(3)
-
-	e.add_child(h)
-	f.add_child(i)
-	g.add_child(j)
-
-	k = DominoTree(9)
-	l = DominoTree(10)
-	m = DominoTree(10)
-	n = DominoTree(4)
-
-	h.add_child(k)
-	h.add_child(l)
-	j.add_child(m)
-	j.add_child(n)
-
-	print_string=str(start.value)
-	for x in start.children:
-		save = print_string
-		print_string+= "-"
-		print_string+=str(x.value)
-		if x.children==[]:				
-			print(print_string)
-			print_string=save
-		for y in x.children:
-			save = print_string
-			print_string+=("-")
-			print_string+=str(y.value)
-			if y.children==[]:				
-				print(print_string)
-				print_string=save
-			for z in y.children:
-				save = print_string
-				print_string+=("-")
-				print_string+=str(z.value)
-				if z.children==[]:				
-					print(print_string)
-					print_string=save
-				for zz in z.children:
-					save = print_string
-					print_string+=("-")
-					print_string+=str(zz.value)	
-					if zz.children==[]:				
-						print(print_string)
-						print_string=save
+	#print(start)
